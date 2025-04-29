452706e7bd137d044e8898108a1de1a469f15df6
Jaime <jgomezm@tecsup.edu.pe>
2025-04-29 14:49:33 -0500

    Initial Commit

---
+++ apple_lab08_01_Introduction_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  apple_lab08_01_Introduction_SwiftUI
+//
+//  Created by developer on 4/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apple_lab08_01_Introduction_SwiftUI/apple_lab08_01_Introduction_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_lab08_01_Introduction_SwiftUIApp.swift
+//  apple_lab08_01_Introduction_SwiftUI
+//
+//  Created by developer on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct apple_lab08_01_Introduction_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
